ad4ee9a3981b24a2083e9e445383980c6a586d57
FRANCISCO JAVIER FERNÁNDEZ COLLADOS <3605143@alu365.murciaeduca.es>
2024-12-13 20:13:54 +0100

    Ej 8 cadenas tema 3

---
+++ Pr0302/pr0302/pr0302_ej8.py
@@ -0,0 +1,7 @@
+"""
+Escribir un programa que pregunte por consola el precio de un producto en euros con dos decimales y muestre por
+pantalla el número de euros y el número de céntimos del precio introducido.
+"""
+precio = float(input("Introduce el precio del producto con dos decimales: "))
+
+print(f"El precio introducido es {precio:.2f}")
